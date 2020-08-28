2dbb2f3fbd5b99349d93d22f95e1dbe4d5edebf1
Emin Mamedov <emin.mamedov0914@gmail.com>
2020-08-28 11:01:58 +0300

    Initial commit

---
+++ src/main/java/EchoBot.java
@@ -0,0 +1,36 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class EchoBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotToken() {
+        return "1187501463:AAG3lfC7f4DGiG2-lQs_U7VwXX9NIbgLmpQ";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        try {
+            if (update.hasMessage() && update.getMessage().hasText()) {
+                Message inMessage = update.getMessage();
+                SendMessage outMessage = new SendMessage();
+
+                outMessage.setChatId(inMessage.getChatId());
+
+                outMessage.setText(inMessage.getText());
+
+                execute(outMessage);
+            }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "f1rstExampleBot";
+    }
+}
